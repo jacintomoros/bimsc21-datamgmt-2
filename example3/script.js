45245151af0af6d9cefb778cb6305c7b5b1a0b7c
Furgoljazzer <jacinto.moros.montanes@students.iaac.net>
2022-01-20 21:22:07 +0100

    update camarea view

--- example3/script.js
+++ example3/script.js
@@ -20,6 +20,8 @@ function init() {
     scene.background = new THREE.Color(1,1,1)
     camera = new THREE.PerspectiveCamera( 1000, window.innerWidth / window.innerHeight, 0.1, 1000 )
     camera.position.z = 100
+    camera.position.x = 500
+    camera.position.y = 10
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
